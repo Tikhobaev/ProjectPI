c6474c1b5730c0926c24379c43680ec8779a04c2
Ilya Solovyev <ilyasolovyov52@yandex.ru>
2022-03-30 01:36:04 +0300

    Some tests

--- WeatherMapTests/Mocks/Mock.generated.swift
+++ WeatherMapTests/Mocks/Mock.generated.swift
@@ -1,11 +1,3 @@
-// Generated using Sourcery 1.6.0 — https://github.com/krzysztofzablocki/Sourcery
-// DO NOT EDIT
-
-
-// Generated with SwiftyMocky 4.1.0
-// Required Sourcery: 1.6.0
-
-
 import SwiftyMocky
 import XCTest
 import CoreLocation
